66d68550a9a3bc778ba84843d2aa051436198aa3
siddhant <siddhantp736@gmail.com>
2021-10-30 12:51:19 +0530

    components initial styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './styles/app.scss';
+import Song from './components/Song';
+import Player from './components/Player';
 function App() {
   return (
     <div className="App">
-      <h1>Music App, Hello!!</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
